f4cc758ca782eb77a821f60039dca76d85c03a3e
neemoh <nima.en@gmail.com>
2017-04-11 13:14:24 -0700

    add torsion elastic constraint and some clean up

--- src/ActiveConstraintEnforcementMethods.cpp
+++ src/ActiveConstraintEnforcementMethods.cpp
@@ -54,12 +54,14 @@ using namespace toolbox;
 //-----------------------------------------------------------------------
 // Constructor
 //-----------------------------------------------------------------------
-acPlast::acPlast(double F_MAX_in, double ELASTIC_LENGTH_in, double BOUNDARY_THRESHOLD_in){
-	F_MAX 				= F_MAX_in;
-	ELASTIC_LENGTH 		= ELASTIC_LENGTH_in;
-	BOUNDARY_THRESHOLD 	= BOUNDARY_THRESHOLD_in;
-	p_tool_last 		= KDL::Vector(0.0,0.0,0.0);
-	q					= KDL::Vector(0.0,0.0,0.0);
+acPlast::acPlast(const double F_MAX,
+                 const double ELASTIC_LENGTH,
+                 const double BOUNDARY_THRESHOLD ){
+    F_MAX_ 				= F_MAX;
+    ELASTIC_LENGTH_ 		= ELASTIC_LENGTH;
+    BOUNDARY_THRESHOLD_ 	= BOUNDARY_THRESHOLD;
+    p_tool_last_ 		= KDL::Vector(0.0,0.0,0.0);
+    q_					= KDL::Vector(0.0,0.0,0.0);
 
 }
 
@@ -68,48 +70,52 @@ acPlast::acPlast(double F_MAX_in, double ELASTIC_LENGTH_in, double BOUNDARY_THRE
 // CORRESPONDING PAPER
 //-----------------------------------------------------------------------
 
-void acPlast::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, const KDL::Vector p_desired, const KDL::Vector v_msrd){
+void acPlast::getForce(KDL::Vector &f_out,
+                       const KDL::Vector p_tool,
+                       const KDL::Vector p_desired,
+                       const KDL::Vector v_msrd){
 
-	double F = 0.5;
-	double R = F_MAX;
-	int Kk = R/ELASTIC_LENGTH;
-	double Bk = 1.5;
-	double T = 0.001;
-	KDL::Vector cp = p_desired;
+    double F = 0.5;
+    double R = F_MAX_;
+    // TODO: Kk was int, not sure why. changed it to double and has to be tested
+    double Kk = R/ELASTIC_LENGTH_;
+    double Bk = 1.5;
+    double T = 0.001;
+    KDL::Vector cp = p_desired;
 
-	KDL::Vector q_last = q;
-	double cte = Kk + (Bk/T);
-	KDL::Vector ps = p_tool + (Bk * (q - p_tool_last) / (Kk * T + Bk)); // p_tool-d = p_last
-	KDL::Vector ki = ps - cp;
-	KDL::Vector n = ki;
-	normalizeSafe(n, KDL::Vector(0.0,0.0,0.0));
+    KDL::Vector q_last = q_;
+    double cte = Kk + (Bk/T);
+    KDL::Vector ps = p_tool + (Bk * (q_ - p_tool_last_) / (Kk * T + Bk)); // p_tool-d = p_last
+    KDL::Vector ki = ps - cp;
+    KDL::Vector n = ki;
+    normalizeSafe(n, KDL::Vector(0.0,0.0,0.0));
 
-	double a1 = saturate(0.0, dot(n,ki), (R/cte));
-	double a2 = saturate(0.0, -dot(n,ki), (R/cte));
+    double a1 = saturate(0.0, dot(n,ki), (R/cte));
+    double a2 = saturate(0.0, -dot(n,ki), (R/cte));
 
-	KDL::Vector e = q_last - ps;
-	double a3 = saturate(-a1, dot(n,e), a2);
+    KDL::Vector e = q_last - ps;
+    double a3 = saturate(-a1, dot(n,e), a2);
 
-	//I-n*n'
-	KDL::Vector In1 = KDL::Vector(1 - n[0]*n[0], 0 - n[0]*n[1], 0 - n[0]*n[2]);
-	KDL::Vector In2 = KDL::Vector(0 - n[1]*n[0], 1 - n[1]*n[1], 0 - n[1]*n[2]);
-	KDL::Vector In3 = KDL::Vector(0 - n[2]*n[0], 0 - n[2]*n[1], 1 - n[2]*n[2]);
-	//(I-n*n')*e
-	KDL::Vector mat = KDL::Vector(dot(In1,e), dot(In2,e), dot(In3,e));
-	double m = std::max(1.0,( ( cte/F ) * sqrt( ( vec_norm(e)*vec_norm(e) ) - ( dot(n,e)*dot(n,e) ) ) ) );
+    //I-n*n'
+    KDL::Vector In1 = KDL::Vector(1 - n[0]*n[0], 0 - n[0]*n[1], 0 - n[0]*n[2]);
+    KDL::Vector In2 = KDL::Vector(0 - n[1]*n[0], 1 - n[1]*n[1], 0 - n[1]*n[2]);
+    KDL::Vector In3 = KDL::Vector(0 - n[2]*n[0], 0 - n[2]*n[1], 1 - n[2]*n[2]);
+    //(I-n*n')*e
+    KDL::Vector mat = KDL::Vector(dot(In1,e), dot(In2,e), dot(In3,e));
+    double m = std::max(1.0,( ( cte/F ) * sqrt( ( vec_norm(e)*vec_norm(e) ) - ( dot(n,e)*dot(n,e) ) ) ) );
 
-	q = ps + n * a3 + mat/m;
+    q_ = ps + n * a3 + mat/m;
 
-	KDL::Vector a4 = saturate_vec(e, (F/cte));
-	KDL::Vector qf = ps + a4;
-	double qfql = vec_norm(qf - q_last);
-	double qql = vec_norm(q - q_last);
+    KDL::Vector a4 = saturate_vec(e, (F/cte));
+    KDL::Vector qf = ps + a4;
+    double qfql = vec_norm(qf - q_last);
+    double qql = vec_norm(q_ - q_last);
 
-	if ( qfql <= qql ) q = qf;
+    if ( qfql <= qql ) q_ = qf;
 
-	f_out = (Kk * (q - p_tool) + Bk * ( ( q - q_last ) - ( p_tool - p_tool_last ) ) / T);
+    f_out = (Kk * (q_ - p_tool) + Bk * ( ( q_ - q_last ) - ( p_tool - p_tool_last_ ) ) / T);
 
-	p_tool_last = p_tool;
+    p_tool_last_ = p_tool;
 }
 
 
@@ -119,62 +125,64 @@ void acPlast::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, const KDL:
 //-----------------------------------------------------------------------
 // Constructor
 //-----------------------------------------------------------------------
-acPlastRedirect::acPlastRedirect(double F_MAX_in, double ELASTIC_LENGTH_in, double BOUNDARY_THRESHOLD_in){
-
-	F_MAX = F_MAX_in;
-	ELASTIC_LENGTH = ELASTIC_LENGTH_in;
-	BOUNDARY_THRESHOLD = BOUNDARY_THRESHOLD_in;
-	p_tool_last 		= KDL::Vector(0.0,0.0,0.0);
-	z					= KDL::Vector(0.0,0.0,0.0);
-
+acPlastRedirect::acPlastRedirect(const double F_MAX,
+                                 const double ELASTIC_LENGTH,
+                                 const double BOUNDARY_THRESHOLD)
+{
+    F_MAX_ = F_MAX;
+    ELASTIC_LENGTH_ =ELASTIC_LENGTH;
+    BOUNDARY_THRESHOLD_=BOUNDARY_THRESHOLD;
 }
 
 //-----------------------------------------------------------------------
 // FORCE GENERATION FOLLOWING THE EQUATIONS DESCRIBED IN THE
 // CORRESPONDING PAPER
 //-----------------------------------------------------------------------
-void acPlastRedirect::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, const KDL::Vector p_desired, const KDL::Vector v_msrd){
+void acPlastRedirect::getForce(KDL::Vector &f_out,
+                               const KDL::Vector p_tool,
+                               const KDL::Vector p_desired,
+                               const KDL::Vector v_msrd){
 
-	double sig2 = 2.5;
-	double fc = F_MAX;
-	double theta = 0.4;
-	double sig0 = fc/ELASTIC_LENGTH;
-	double zcss = fc/sig0;
-	KDL::Vector penet = p_tool - p_desired ;
+    double sig2 = 2.5;
+    double fc = F_MAX_;
+    double theta = 0.4;
+    double sig0 = fc/ELASTIC_LENGTH_;
+    double zcss = fc/sig0;
+    KDL::Vector penet = p_tool - p_desired ;
 
-	// boundary condition
-	double ptrans = BOUNDARY_THRESHOLD;
-	if ( vec_norm(penet) < ptrans ) theta = theta * (vec_norm(penet)/ptrans);
+    // boundary condition
+    double ptrans = BOUNDARY_THRESHOLD_;
+    if ( vec_norm(penet) < ptrans ) theta = theta * (vec_norm(penet)/ptrans);
 
-	z = z + p_tool- p_tool_last;
+    z_ = z_ + p_tool- p_tool_last_;
 
-	KDL::Vector ptn = penet;
-	normalizeSafe(ptn, KDL::Vector(0.0,0.0,0.0));
-	KDL::Vector zn = z;
-	normalizeSafe(zn, KDL::Vector(0.0,0.0,0.0));
-	double a = atan2(vec_norm(ptn*zn), dot(zn, ptn));
+    KDL::Vector ptn = penet;
+    normalizeSafe(ptn, KDL::Vector(0.0,0.0,0.0));
+    KDL::Vector zn = z_;
+    normalizeSafe(zn, KDL::Vector(0.0,0.0,0.0));
+    double a = atan2(vec_norm(ptn*zn), dot(zn, ptn));
 
-	KDL::Vector n = penet*z;
-	KDL::Vector nn = n;
-	normalizeSafe(nn, KDL::Vector(0.0,0.0,0.0));
+    KDL::Vector n = penet*z_;
+    KDL::Vector nn = n;
+    normalizeSafe(nn, KDL::Vector(0.0,0.0,0.0));
 
-	KDL::Vector y = cos(theta)*penet + sin(theta)*(nn*penet) + (1-cos(theta))*dot(nn,penet)*nn;
-	KDL::Vector yn = y;
-	normalizeSafe(yn, KDL::Vector(0.0,0.0,0.0));
+    KDL::Vector y = cos(theta)*penet + sin(theta)*(nn*penet) + (1-cos(theta))*dot(nn,penet)*nn;
+    KDL::Vector yn = y;
+    normalizeSafe(yn, KDL::Vector(0.0,0.0,0.0));
 
-	if ( a <= theta && vec_norm(z) <= zcss )
-		z = z * 1;
-	else if ( a <= theta && vec_norm(z) > zcss )
-		z = zcss*zn;
-	else
-	{
-		if ( dot(z,yn) <= 0.0 ) z = KDL::Vector(0.0,0.0,0.0);
-		else if ( (0.0 < dot(z,yn)) && (dot(z,yn) < zcss) ) z = dot(z,yn)*yn;
-		else z = zcss * yn;
-	}
+    if ( a <= theta && vec_norm(z_) <= zcss )
+        z_ = z_ * 1;
+    else if ( a <= theta && vec_norm(z_) > zcss )
+        z_ = zcss*zn;
+    else
+    {
+        if ( dot(z_,yn) <= 0.0 ) z_ = KDL::Vector(0.0,0.0,0.0);
+        else if ( (0.0 < dot(z_,yn)) && (dot(z_,yn) < zcss) ) z_ = dot(z_,yn)*yn;
+        else z_ = zcss * yn;
+    }
 
-	f_out =  -(sig0*z + sig2*v_msrd);
-	p_tool_last = p_tool;
+    f_out =  -(sig0*z_ + sig2*v_msrd);
+    p_tool_last_ = p_tool;
 }
 
 
@@ -185,14 +193,18 @@ void acPlastRedirect::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, co
 //-----------------------------------------------------------------------
 // Constructor
 //-----------------------------------------------------------------------
-acViscousRedirect::acViscousRedirect(double F_MAX_in, double B_MAX_in, double BOUNDARY_THRESHOLD_in){
+acViscousRedirect::acViscousRedirect(double F_MAX_in,
+                                     double B_MAX_in,
+                                     double BOUNDARY_THRESHOLD_in){
 
-	F_MAX = F_MAX_in;
-	B_MAX = B_MAX_in;
-	BOUNDARY_THRESHOLD = BOUNDARY_THRESHOLD_in;
-	v_dir_last = KDL::Vector(1.0,0.0,0.0);
-	f_dir_last = KDL::Vector(1.0,0.0,0.0);
-	n_2_last   = KDL::Vector(1.0,0.0,0.0);
+    F_MAX_ = F_MAX_in;
+    B_MAX_ = B_MAX_in;
+    BOUNDARY_THRESHOLD_ = BOUNDARY_THRESHOLD_in;
+
+
+    v_dir_last_ = KDL::Vector(1.0,0.0,0.0);
+    f_dir_last_ = KDL::Vector(1.0,0.0,0.0);
+    n_2_last_   = KDL::Vector(1.0,0.0,0.0);
 
 }
 
@@ -201,59 +213,62 @@ acViscousRedirect::acViscousRedirect(double F_MAX_in, double B_MAX_in, double BO
 // CORRESPONDING PAPER
 //-----------------------------------------------------------------------
 
-void acViscousRedirect::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, const KDL::Vector p_desired, const KDL::Vector v_msrd){
+void acViscousRedirect::getForce(KDL::Vector &f_out,
+                                 const KDL::Vector p_tool,
+                                 const KDL::Vector p_desired,
+                                 const KDL::Vector v_msrd){
 
-	// UPPERCASE NAMES = SCALARS
-	// LOWERCASE NAMES = VECTORS
+    // UPPERCASE NAMES = SCALARS
+    // LOWERCASE NAMES = VECTORS
 
-	KDL::Vector penet = p_desired - p_tool;
-	double F_VC, F_VC_SAT;
-	double B_M			= B_MAX;
+    KDL::Vector penet = p_desired - p_tool;
+    double F_VC, F_VC_SAT;
+    double B_M			= B_MAX_;
 
-	KDL::Vector  f_dir, penet_dir, v_tool_dir;
+    KDL::Vector  f_dir, penet_dir, v_tool_dir;
 
-	penet_dir 		= penet;
-	v_tool_dir 		= v_msrd;
+    penet_dir 		= penet;
+    v_tool_dir 		= v_msrd;
 
-	normalizeSafe(v_tool_dir, v_dir_last);
-	normalizeSafe(penet_dir, KDL::Vector(1.0,0.0,0.0));
+    normalizeSafe(v_tool_dir, v_dir_last_);
+    normalizeSafe(penet_dir, KDL::Vector(1.0,0.0,0.0));
 
-	double V_PENET_DOTP = dot(v_tool_dir, penet_dir);
-	double PENET =vec_norm(penet);
+    double V_PENET_DOTP = dot(v_tool_dir, penet_dir);
+    double PENET =vec_norm(penet);
 
-	// limit the viscous coefficient around the boundary to minimize the oscillation
-	if (PENET< BOUNDARY_THRESHOLD){
-		B_M =  B_MAX * PENET/ BOUNDARY_THRESHOLD;
-	}
-	else
-		B_M = B_MAX;
+    // limit the viscous coefficient around the boundary to minimize the oscillation
+    if (PENET< BOUNDARY_THRESHOLD_){
+        B_M =  B_MAX_ * PENET/ BOUNDARY_THRESHOLD_;
+    }
+    else
+        B_M = B_MAX_;
 
-	// calculate the magnitude of the ac force
+    // calculate the magnitude of the ac force
 //	F_VC = B_M * sqrt( ( 1 - V_PENET_DOTP ) / 2 ) * v_msrd.Norm();
-	F_VC = B_M * sqrt( ( 1 - V_PENET_DOTP ) / 2 ) * vec_norm(v_msrd);
-	// saturate the magnitude of the ac force
-	F_VC_SAT = saturate(0.0, F_VC , F_MAX );
+    F_VC = B_M * sqrt( ( 1 - V_PENET_DOTP ) / 2 ) * vec_norm(v_msrd);
+    // saturate the magnitude of the ac force
+    F_VC_SAT = saturate(0.0, F_VC , F_MAX_ );
 
-	//-----------------------------------------------------------------------
-	// calculate the direction of the AC force
-	KDL::Vector n = v_tool_dir * penet_dir;
-	KDL::Vector nn = n;
-	normalizeSafe(nn, penet_dir);
+    //-----------------------------------------------------------------------
+    // calculate the direction of the AC force
+    KDL::Vector n = v_tool_dir * penet_dir;
+    KDL::Vector nn = n;
+    normalizeSafe(nn, penet_dir);
 
-	double THETA = (M_PI/2) * (1 + V_PENET_DOTP);
+    double THETA = (M_PI/2) * (1 + V_PENET_DOTP);
 
-	if ( V_PENET_DOTP < 0.0 )
-		f_dir = penet_dir;
-	else
-		if(rotateVector(v_tool_dir, f_dir, nn, THETA) < 0)
-			std::cout << "Null in rotateVector." <<"  norm(v_tool_dir) = "<< vec_norm(v_tool_dir)<<
-			"  , norm(nn) = "<< vec_norm(nn) << std::endl;
+    if ( V_PENET_DOTP < 0.0 )
+        f_dir = penet_dir;
+    else
+    if(rotateVector(v_tool_dir, f_dir, nn, THETA) < 0)
+        std::cout << "Null in rotateVector." <<"  norm(v_tool_dir) = "<< vec_norm(v_tool_dir)<<
+                  "  , norm(nn) = "<< vec_norm(nn) << std::endl;
 
-	//-----------------------------------------------------------------------
-	// Make the force vector from the calculated magnitude and direction
-	f_out = F_VC_SAT * f_dir;
+    //-----------------------------------------------------------------------
+    // Make the force vector from the calculated magnitude and direction
+    f_out = F_VC_SAT * f_dir;
 
-	v_dir_last = v_tool_dir;
+    v_dir_last_ = v_tool_dir;
 
 }
 
@@ -266,13 +281,18 @@ void acViscousRedirect::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, 
 //-----------------------------------------------------------------------
 // Constructor
 //-----------------------------------------------------------------------
-acElastic::acElastic(double _F_MAX, double _k, double _b, double _dt){
-	F_MAX = _F_MAX;
-	k = _k;
-	b = _b;
-	dt = _dt;
-	penet_last = KDL::Vector(0.0, 0.0, 0.0);
-	penet_vel = 0;
+acElastic::acElastic(const double F_MAX,
+                     const double TAW_MAX,
+                     const double k,
+                     const double b,
+                     const double kappa) {
+    F_MAX_ = F_MAX;
+    TAW_MAX_ = TAW_MAX;
+    k_ = k;
+    b_ = b;
+    kappa_ = kappa;
+    penet_vel_ = 0;
+
 };
 
 
@@ -280,27 +300,57 @@ acElastic::acElastic(double _F_MAX, double _k, double _b, double _dt){
 // FORCE GENERATION
 //-----------------------------------------------------------------------
 
-void acElastic::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, const KDL::Vector p_desired, const KDL::Vector v_msrd){
+void acElastic::getForce(KDL::Vector &f_out,
+                         const KDL::Vector p_tool,
+                         const KDL::Vector p_desired,
+                         const KDL::Vector v_msrd){
 
-	// find the penetration vector
-	KDL::Vector penet = p_desired - p_tool;
+    // find the penetration vector
+    KDL::Vector penet = p_desired - p_tool;
 
-	// make the force vector
-	KDL::Vector f_all = k * penet - b * v_msrd;
-	double f_all_magnitude = toolbox::vec_norm(f_all);
+    // make the force vector
+    KDL::Vector f_all = k_ * penet - b_ * v_msrd;
+    double f_all_magnitude = toolbox::vec_norm(f_all);
 
-	// limit the force to F_MAX
-	if (f_all_magnitude > F_MAX)
-		f_out = F_MAX/f_all_magnitude * f_all;
-	else
-		f_out = f_all;
+    // limit the force to F_MAX_
+    if (f_all_magnitude > F_MAX_)
+        f_out = F_MAX_/f_all_magnitude * f_all;
+    else
+        f_out = f_all;
 
-	// save last penet
-	penet_last = penet;
+    // save last penet
+    penet_last = penet;
 
 }
 
 
+//-----------------------------------------------------------------------
+// FORCE TORQUE
+//-----------------------------------------------------------------------
+
+void acElastic::getTorque(KDL::Vector &taw_out,
+                          const KDL::Rotation rot_current,
+                          const KDL::Rotation rot_desired,
+                          const KDL::Vector rot_vel){
+
+    // find the delta rotation
+    KDL::Rotation d_rot = rot_desired * rot_current.Inverse();
+
+    // convert to RPY
+    KDL::Vector delta_rpy;
+    d_rot.GetRPY(delta_rpy[0], delta_rpy[1], delta_rpy[2]);
+
+    // find the elastic torque
+    KDL::Vector taw_all = kappa_ * delta_rpy; //- b_ * rot_vel;
+    double taw_norm = taw_all.Norm();
+
+    // limit the force to F_MAX_
+    if (taw_norm > TAW_MAX_)
+        taw_out = TAW_MAX_/taw_norm * taw_all;
+    else
+        taw_out = taw_all;
+
+}
 
 
 
@@ -311,59 +361,68 @@ void acElastic::getForce(KDL::Vector &f_out,  const KDL::Vector p_tool, const KD
 // #############################################################################
 //##############################################################################
 
+int toolbox::rotateVector(const KDL::Vector vector_in,
+                          KDL::Vector &vector_out,
+                          const KDL::Vector n,
+                          const double THETA){
 
-int toolbox::rotateVector(const KDL::Vector vector_in, KDL::Vector &vector_out,  const KDL::Vector n, const double THETA){
-	if (vec_norm(vector_in) ==0.0 || vec_norm(n) ==0.0){
+    if (vec_norm(vector_in) ==0.0 || vec_norm(n) ==0.0){
 //		ROS_ERROR("Null vector given as the input of rotation function.");
-		return -1;
-	}
-	else{
-		vector_out = vector_in + sin(THETA)*(n*vector_in)+(1.0-cos(THETA))*(n*(n*vector_in));
-		return 0;
-	}
+        return -1;
+    }
+    else{
+        vector_out = vector_in + sin(THETA)*(n*vector_in)+(1.0-cos(THETA))*(n*(n*vector_in));
+        return 0;
+    }
 }
-int toolbox::normalizeSafe(KDL::Vector & vec_in, const KDL::Vector vec_safe) {
-	// Checks the norm of the vec_in. If it is non zero the function normalizes it.
-	// If the norm is zero the function uses vec_safe as the output
+int toolbox::normalizeSafe(KDL::Vector & vec_in,
+                           const KDL::Vector vec_safe) {
 
-	double l = vec_norm(vec_in);
-	if( fabs(l) >= std::numeric_limits<double>::epsilon() ) {
-		vec_in[0] /= l;
-		vec_in[1] /= l;
-		vec_in[2] /= l;
-		return 0;
-	} else {
+    // Checks the norm of the vec_in. If it is non zero the function normalizes it.
+    // If the norm is zero the function uses vec_safe as the output
 
-		vec_in = vec_safe;
-		return -1;
-	}
+    double l = vec_norm(vec_in);
+    if( fabs(l) >= std::numeric_limits<double>::epsilon() ) {
+        vec_in[0] /= l;
+        vec_in[1] /= l;
+        vec_in[2] /= l;
+        return 0;
+    } else {
+
+        vec_in = vec_safe;
+        return -1;
+    }
 }
 
-double toolbox::saturate (double a, double x, double b){
+double toolbox::saturate (double a,
+                          double x,
+                          double b){
 
-	if (x < a){
-		return a;
-	}
-	else if (x >b){
-		return b;
-	}
-	else
-		return x;
+    if (x < a){
+        return a;
+    }
+    else if (x >b){
+        return b;
+    }
+    else
+        return x;
 
 }
-KDL::Vector toolbox::saturate_vec(KDL::Vector x, double a){
 
-	double l = vec_norm(x);
-	KDL::Vector xn = x;
-	normalizeSafe(xn, KDL::Vector(0.0,0.0,0.0));
+KDL::Vector toolbox::saturate_vec(KDL::Vector x,
+                                  double a){
 
-	if (l <= a) return x;
-	else return a * xn;
+    double l = vec_norm(x);
+    KDL::Vector xn = x;
+    normalizeSafe(xn, KDL::Vector(0.0,0.0,0.0));
+
+    if (l <= a) return x;
+    else return a * xn;
 
 }
 
 double toolbox::vec_norm(KDL::Vector x) {
-	return sqrt( x[0]*x[0] + x[1]*x[1] + x[2]*x[2] );
+    return sqrt( x[0]*x[0] + x[1]*x[1] + x[2]*x[2] );
 }
 
 
